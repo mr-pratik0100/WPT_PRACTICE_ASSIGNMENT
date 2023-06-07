69cb40cea68748688b0b20bee6a6da4b9ca28444
pratik-0100 <pratik.shiradkar@digismartassets.com>
2023-06-07 21:29:38 +0530

    initial commit

---
+++ Day_5_validation/p1.js
@@ -0,0 +1,13 @@
+
+
+
+
+var d=new Date();
+var ans=d.getMonth();
+console.log(ans);
+// var d1=new Date().toLocaleTimeString();
+// var d3=new Date();
+// var ans =d3.toLocaleString();
+// console.log(ans);
+// console.log(d)
+// console.log(d1);
